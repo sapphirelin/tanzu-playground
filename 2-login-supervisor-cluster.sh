824d0a42790228ac2bea8e9868b9321daa4cdbab
Sapphire Lin <30453370+sapphirelin@users.noreply.github.com>
2022-01-24 21:34:45 +0800

    Update 2-login-supervisor-cluster.sh

--- 2-login-supervisor-cluster.sh
+++ 2-login-supervisor-cluster.sh
@@ -4,13 +4,10 @@ export SCP=192.168.30.34
 export TKC_NS=first-ns
 export VSPHERE_USERNAME=administrator@vsphere.local
 export KUBECTL_VSPHERE_PASSWORD=VMware1!
-export TKC=tkc-01
 
 kubectl vsphere login \
 --server=$SCP \
---tanzu-kubernetes-cluster-namespace=$TKC_NS \
---tanzu-kubernetes-cluster-name=$TKC \
 --vsphere-username=$VSPHERE_USERNAME \
 --insecure-skip-tls-verify
 
-kubectl config use-context $TKC
+kubectl config use-context $TKC_NS
