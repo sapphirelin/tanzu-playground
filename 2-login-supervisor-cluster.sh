c7a7d815188f2364e0c64280287669452e08fe20
Sapphire Lin <30453370+sapphirelin@users.noreply.github.com>
2022-03-18 18:16:54 +0800

    Update 2-login-supervisor-cluster.sh

--- 2-login-supervisor-cluster.sh
+++ 2-login-supervisor-cluster.sh
@@ -1,7 +1,7 @@
 ## 2. Login to wcp (Tanzu Supervisior Cluster)
 
 export SCP=192.168.30.34
-export TKC_NS=first-ns
+export TKC_NS=first-ns ## 於 vCenter UI 上建立
 export VSPHERE_USERNAME=administrator@vsphere.local
 export KUBECTL_VSPHERE_PASSWORD=VMware1!
 
